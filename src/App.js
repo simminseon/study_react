6386dc2469b8fc2d329d26544346d8015b68c8cf
sunny.sim <sunny.sim@kakaocorp.com>
2020-11-06 16:52:28 +0900

    예제 수정 적용

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Test from './test/Test';
+import Greeting from './test/Greeting';
 
 
 function App() {
   return (
     <div className="App">
       <Test />
+      <Greeting isLoggedIn={false} />
     </div>
   );
 }
--- src/test/Test.jsx
+++ src/test/Test.jsx
@@ -1,18 +1,88 @@
 // import React from 'react';
 import React, { useState } from 'react';
 
-function Test() {
-    const [toggle, setToggle] = useState(true);
 
+// class Test extends React.Component{
+//     constructor(props) {
+//         super(props);
+//         this.state = {isToggleOn: true};
+    
+//         // 콜백에서 `this`가 작동하려면 아래와 같이 바인딩 해주어야 합니다.
+//         this.handleClick = this.handleClick.bind(this);
+//       }
+    
+//       handleClick() {
+//         this.setState(state => ({
+//           isToggleOn: !state.isToggleOn
+//         }));
+//       }
+    
+//       render() {
+//         return (
+//           <button onClick={this.handleClick}>
+//             {this.state.isToggleOn ? 'ON' : 'OFF'}
+//           </button>
+//         );
+//       }
+// }
+
+// function Test() {
+//     const [toggleText, setToggleText] = useState(false);
+//     const isToggleText = toggleText ? "OFF" : "ON"
+
+//     function handleClick() {
+//         setToggleText(!toggleText);
+//     }
+
+//     return (
+//         <button onClick={handleClick}>
+//             {isToggleText}
+//         </button>
+//     );
+// }
+
+
+
+function Test() {
+    const [toggleText, setToggleText] = useState(false);
+    
     function handleClick() {
-        const text = () => toggle ? console.log(true) : console.log(false);
-        console.log(text)
+        setToggleText(!toggleText);
     }
+
+    return <TestButton event={handleClick} text={toggleText} /> 
+}
+
+function TestButton({event, text}) {
+    const isToggleText = text ? "OFF" : "ON";
+
     return (
-        <button onClick={handleClick}>
-            {toggle}
+        <button onClick={event}>
+            {isToggleText}
         </button>
     );
 }
+
+// --------------------------------------------------------------
+
+// function Test() {
+//     const [toggleText, setToggleText] = useState(false);
+//     const isToggleText = toggleText ? "OFF" : "ON";
+//     const exportToggle = {event: handleClick, text: isToggleText}
+
+//     function handleClick() {
+//         setToggleText(!toggleText);
+//     }
+
+//     return <TestButton {...exportToggle} /> 
+// }
+
+// function TestButton({event, text}) {
+//     return (
+//         <button onClick={event}>
+//             {text}
+//         </button>
+//     );
+// }
 
 export default Test;
